2c2ddc17d5633440c3141fcaf02dec6dc3d50a52
Vikash Mishra <vikash.mishra@BRILLIO.COM>
2023-05-23 11:15:15 +0530

    dempavro

---
+++ demo/src/main/java/com/example/avro/kafka/consumer/SpringBootAvroConsumer.java
@@ -0,0 +1,19 @@
+package com.example.avro.kafka.consumer;
+
+import com.example.avro.schema.StockHistory;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringBootAvroConsumer {
+
+    @KafkaListener(topics = "${avro.topic.name}", containerFactory = "kafkaListenerContainerFactory")
+    public void read(ConsumerRecord<String, StockHistory> record){
+        String key=record.key();
+        StockHistory history=record.value();
+
+        System.out.println("Avro message received for key : "+key+ " value : "+history.toString());
+    }
+
+}
---
+++ demo/src/main/java/com/example/avro/kafka/controller/SpringAvroRestController.java
@@ -0,0 +1,31 @@
+package com.example.avro.kafka.controller;
+
+import com.example.avro.kafka.model.StockHistoryModel;
+import com.example.avro.kafka.producer.SpringAvroProducer;
+import com.example.avro.schema.StockHistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class SpringAvroRestController {
+
+    @Autowired
+    private SpringAvroProducer springAvroProducer;
+
+    @PostMapping(value = "/sendStockHistory")
+    public void sendStockHistory(@RequestBody StockHistoryModel model){
+        StockHistory stockHistory= StockHistory.newBuilder().build();
+        stockHistory.setStockName(model.getStockName());
+        stockHistory.setTradeType(model.getTradeType());
+        stockHistory.setPrice(model.getPrice());
+        stockHistory.setAmount(model.getAmount());
+        stockHistory.setTradeId(new Random(1000).nextInt());
+        stockHistory.setTradeMarket(model.getTradeMarket());
+        stockHistory.setTradeQuantity(model.getTradeQuantity());
+        springAvroProducer.send(stockHistory);
+    }
+}
